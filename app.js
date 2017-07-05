4e846eb0b4d846d18cf4d08746eff2e5a39f3a36
vivian <lincc7979@gmail.com>
2017-07-05 19:50:06 +0800

    add v1.4

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(async (ctx, next) => {
 
 
 // static file support:
-if (! isProduction) {
+if (isProduction) {
     let staticFiles = require('./static-files');
     app.use(staticFiles('/static/', __dirname + '/static'));
 }
